d1e987c5592a2c72391d2d59780305517b92a657
Hexcold <henriquepiresh3@gmail.com>
2024-03-10 15:52:12 -0300

    atulização das questões

--- base/L0 - Dividindo resultado e @resto/C/Solver.c
+++ base/L0 - Dividindo resultado e @resto/C/Solver.c
@@ -1,3 +1,9 @@
+// autor: Henique L. Pires (instagram: @henrq.p | github: Hexcold)
+
+// Repositório online: https://github.com/Hexcold
+// Repositório original das questões: https://github.com/qxcodefup/arcade/blob/master/base/resto/Readme.md
+
+
 #include <stdio.h> //Incluir a biblioteca “stdio.h”
 
 int main()
--- base/L0 - Soma @tres inteiros/Python/Solver.py
+++ base/L0 - Soma @tres inteiros/Python/Solver.py
@@ -1,3 +1,8 @@
+# autor: Henique L. Pires (instagram: @henrq.p | github: Hexcold)
+
+# Repositório online: https://github.com/Hexcold
+# Repositório original das questões: https://github.com/qxcodefup/arcade/blob/master/base/resto/Readme.md
+
 entrada = input()
 primeiroNumero = int(entrada)
 
