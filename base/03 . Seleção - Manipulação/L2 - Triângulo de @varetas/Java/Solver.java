cbf8f8f0d39b37ef1dacd7619c8c59557c267841
Hexcold <henriquepiresh3@gmail.com>
2024-03-27 18:29:30 -0300

    Adição das pastas de JAVA, JAVASCRIPT e TYPESCRIPT | Atualizção dos Makefile, para as novas linguagens

---
+++ base/03 . Seleção - Manipulação/L2 - Triângulo de @varetas/Java/Solver.java
@@ -0,0 +1,11 @@
+// autor: Henique L. Pires (instagram: @henrq.p | github: Hexcold)
+
+// Repositório online: https://github.com/Hexcold
+// Repositório original das questões: https://github.com/qxcodefup/arcade/blob/master/base/resto/Readme.md
+
+
+public class Solver {
+    public static void main(String[] args) {
+        //A versão em Java ainda será adicionada futuramente     
+    }
+}
