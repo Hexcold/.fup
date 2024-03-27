103090bb3c6b6a30dc0e0e645f3d07d440497395
Hexcold <henriquepiresh3@gmail.com>
2024-03-27 16:12:44 -0300

    Adição da pasta | 05 . Seleção - Lógica

---
+++ base/05 . Seleção - Lógica/L2 - @jokenpo das tartarugas/Python/Solver.py
@@ -0,0 +1,5 @@
+# autor: Henique L. Pires (instagram: @henrq.p | github: Hexcold)
+
+# Repositório online: https://github.com/Hexcold
+# Repositório original das questões: https://github.com/qxcodefup/arcade/blob/master/base/resto/Readme.md
+
